ad4755051c967891b5b632d46b8aa8e1d9d48dc9
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-07-21 18:20:29 +0300

    Add files via upload

---
+++ 2107hw2/2107hw2/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2107hw2
+{
+    public class Country
+    {
+        public string Capital { get; set; }
+        public string Name { get; set; }
+        public string ResourceFlagUrl { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} {Capital}";
+        }
+    }
+}
